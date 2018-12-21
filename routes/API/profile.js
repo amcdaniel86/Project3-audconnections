ba597dcf91dcbfd64925dcd82313fc846edade2d
Alex McDaniel <amcdaniel186@gmail.com>
2018-12-21 02:01:33 -0500

    profile routes files, worked on

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -41,6 +41,70 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 });
 
+// @route     GET api/profile/all
+// @desc      Get all profiles
+// @access    Public
+router.get('/all', (req, res) => {
+  const errors = {};
+  Profile.find()
+  .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if(!profiles) {
+        errors.noprofile = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => 
+      res.status(404).json({ profile: 'There is no profiles' }));
+});
+
+
+// won't be the word handle, it'll be the specific term
+// @route     GET api/profile/handle/:handle
+// @desc      Get profile by handle
+// @access    Public
+// Private routes require the passport middleware in first route line.
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+  
+  Profile.findOne({ handle: req.params.handle })
+  .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user.';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// won't be the word handle, it'll be the specific term
+// @route     GET api/profile/user/:id
+// @desc      Get profile by user ID
+// @access    Public
+// Private routes require the passport middleware in first route line.
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+  
+  Profile.findOne({ user: req.params.user_id })
+  .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user.';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json({profile: 'There is no profile for this user'}));
+});
+
+
+
 // @route     POST api/profile
 // @desc      Create or Edit user profile
 // @access    Private
